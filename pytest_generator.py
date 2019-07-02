faa513a99b17ebc9fb393f3138609596984065d2
GuitarBuilderClass <wordswrod.mk@gmail.com>
2019-07-03 02:48:27 +0900

    ✨ : Add new function for AtCoder
    fetch HTML of the problem

--- pytest_generator.py
+++ pytest_generator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from abc import ABCMeta, abstractmethod
 
+import requests
 import yaml
+from bs4 import BeautifulSoup
 
 
 class Questioner:
@@ -11,19 +13,23 @@ class Questioner:
 
 class PyTestGenerator(metaclass=ABCMeta):
     def __init__(self, is_test=False):
+        """
+        Args:
+            is_test: (bool) Are you testing?
+        """
         if is_test:
             self.config_file = "./config_test.yml"
         else:
             self.config_file = "./config.yml"
         with open(self.config_file, encoding='utf-8') as file:
-            self.config = yaml.load(file)
+            self.config = yaml.safe_load(file)
 
     @abstractmethod
     def login(self):
         pass
 
     @abstractmethod
-    def fetch_html(self):
+    def fetch_html(self, url):
         pass
 
     @abstractmethod
@@ -36,16 +42,53 @@ class PyTestGenerator(metaclass=ABCMeta):
 
 
 class AtCoderTestGenerator(PyTestGenerator):
+    """For AtCoder's Pytest Generator"""
 
     def __init__(self, is_test=False):
+        """
+        Args:
+            is_test: (bool) Are you testing?
+
+
+        `self.config`: (dict<str, str>)
+            'login': login method; AtCoder only uses a password.
+            'login_url': The `lang=en` argument is meaningless and gives the same result whether or not it is used.
+            'id': login ID
+            'password': login password
+            'csrf_token': Learn not only about algorithms, but also how the Web works (c.f., https://en.wikipedia.org/wiki/Cross-site_request_forgery)
+        """
         super().__init__(is_test)
-        print(self.config["atcoder"])
+        self.config = self.config["AtCoder"]
+
+        self.session = requests.Session()
+        request = self.session.get(self.config["login_url"])
+        soup = BeautifulSoup(request.text, features="html.parser")
+        self.config["csrf_token"] = soup.find(attrs={
+            'name': 'csrf_token'
+        }).get('value')
 
     def login(self):
-        pass
+        """
+        Give AtCoder login information to session.
 
-    def fetch_html(self):
-        pass
+        Returns:
+            None
+        """
+        self.session.post(self.config["login_url"], data=self.config)
+
+    def fetch_html(self, url):
+        """
+        Fetch a HTML source from the URL of the problem statement.
+
+        Args:
+            url: URL of the problem statement page.
+
+        Returns:
+            HTML source
+        """
+        request = self.session.get(url)
+        html = BeautifulSoup(request.text, features="html.parser")
+        return html
 
     def define_sample_set(self):
         pass
@@ -82,5 +125,3 @@ class PyTestFactory:
             return AtCoderTestGenerator(is_test)
         # elif self.questioner == Questioner.paiza:
         #     return PaizaTestGenerator(is_test)
-
-
--- test_data/test_pytest_generator.py
+++ test_data/test_pytest_generator.py
@@ -5,3 +5,6 @@ from pytest_generator import PyTestFactory
 
 if __name__ == '__main__':
     questioner = PyTestFactory("atcoder").create(is_test=True)
+    questioner.login()
+    print(questioner.fetch_html("https://atcoder.jp/contests/abs/tasks/abc086_a"))
+
